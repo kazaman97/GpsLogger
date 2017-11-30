893297d674053989a495aefe274a6c8f5afb0484
narusai <narusai97@gmail.com>
2017-11-30 22:32:03 +0900

    fused locationに切り替え

--- app/src/main/java/com/example/narusai/gpslogger/MapsActivity.java
+++ app/src/main/java/com/example/narusai/gpslogger/MapsActivity.java
@@ -1,18 +1,24 @@
 package com.example.narusai.gpslogger;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.os.Looper;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatCallback;
 import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
@@ -20,7 +26,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -28,15 +45,33 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener/*, CompoundButton.OnCheckedChangeListener*/ {
+import java.text.DateFormat;
+import java.util.Date;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback/*, CompoundButton.OnCheckedChangeListener*/ {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private SettingsClient settingsClient;
+    private LocationSettingsRequest locationSettingsRequest;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+    private Location location;
 
     private GoogleMap mMap;
-    private LocationManager locationManager;
+    private String lastUpdateTime;
+    private int priority = 0;
+    private Boolean requestingLocationUpdates;
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private TextView textView;
+    private String textLog;
+
     // テスト用の座標
     private LatLng mydaigaku = new LatLng(39.802802, 141.137441);
 
-    private LatLng genzai;
+    private LatLng genzai; // サイトでいうLocation
 
     // CompoundButton cmap = (CompoundButton)findViewById(R.id.changeMap);
     // private ToggleButton cmap = (ToggleButton)findViewById(R.id.changeMap);
@@ -52,12 +87,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        settingsClient = LocationServices.getSettingsClient(this);
+
+        createLocationCallback();
+        createLocationRequest();
+        buildLocationSettingsRequest();
+
+        locationUpdates(); // サイトで言うstartLocationUpdates()
+
+        /*
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
         }
         else {
             locationStart();
         }
+        */
         mapFragment.getMapAsync(this);
 
 
@@ -74,6 +120,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }*/
 
+    private void createLocationCallback() {
+        locationCallback = new LocationCallback() {
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                location = locationResult.getLastLocation();
+                lastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+                genzai = new LatLng(location.getLatitude(), location.getLongitude());
+                TextView textView = (TextView)findViewById(R.id.textView1);
+                textView.setText("緯度:"+location.getLatitude()+" "+"経度:"+location.getLongitude());
+                po.add(genzai);
+                mMap.addPolyline(po);
+            }
+        };
+    }
+
+    private void createLocationRequest() {
+        locationRequest = new LocationRequest();
+
+        priority = 0;
+
+        if (priority == 0) {
+            // 高い精度の位置情報を取得したいとき
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        }else if (priority == 1) {
+            // バッテリー消費を抑えたい場合、精度は100mと悪くなる
+            locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        }else if (priority == 2) {
+            // バッテリー消費を抑えたい場合、精度は10kmと悪くなる
+            locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+        }else {
+            // 受け身的な位置情報取得でアプリが自ら測位せず、他のアプリで得られた位置情報は入手できる
+            locationRequest.setPriority(LocationRequest.PRIORITY_NO_POWER);
+        }
+
+        locationRequest.setInterval(60000);
+
+        locationRequest.setFastestInterval(5000);
+    }
+
+    private void buildLocationSettingsRequest(){
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        locationSettingsRequest = builder.build();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.i("debug", "User agreed to make required location settings changes.");
+                        // Nothing to do. startLocationupdates() gets called in onResume again.
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i("debug", "User chose not to make required location settings changes.");
+                        requestingLocationUpdates = false;
+                        break;
+                }
+                break;
+        }
+    }
+    /*
     private void locationStart(){
         Log.d("debag", "locationStart");
 
@@ -100,38 +210,67 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, this);
     }
+    */
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        switch (status) {
-            case LocationProvider.AVAILABLE:
-                Log.d("debug", "LocationProvider.AVAILABLE");
-                break;
-            case LocationProvider.OUT_OF_SERVICE:
-                Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
-                break;
-            case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
-                break;
-        }
+
+
+
+    private void locationUpdates() {
+        // Begin by checking if the device has the necessary location settings.
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        Log.i("debug", "All location settings are satisfied.");
+
+                        // パーミッションの確認
+                        if (ActivityCompat.checkSelfPermission(MapsActivity.this,
+                                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                                && ActivityCompat.checkSelfPermission(MapsActivity.this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i("debug", "Location settings are not satisfied. Attempting to upgrade " +
+                                        "location settings ");
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(MapsActivity.this, REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i("debug", "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                                Log.e("debug", errorMessage);
+                                Toast.makeText(MapsActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                                requestingLocationUpdates = false;
+                        }
+
+                    }
+                });
+
+        requestingLocationUpdates = true;
     }
-
-    public void onLocationChanged(Location location) {
-        genzai = new LatLng(location.getLatitude(), location.getLongitude());
-        TextView textView = (TextView)findViewById(R.id.textView1);
-        textView.setText("緯度:"+location.getLatitude()+" "+"経度:"+location.getLongitude());
-        po.add(genzai);
-        mMap.addPolyline(po);
-    }
-
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    public void onProviderDisabled(String provider) {
-
-    }
-
 
     /**
      * Manipulates the map once available.
